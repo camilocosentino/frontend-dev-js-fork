5e9681a35f174f4225f229624538bf6b370b08ef
Camilo Cosentino <ccosentino@domsf.com>
2022-08-15 23:56:08 -0300

    Fusion Menu Mobile View

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileHamburger = document.querySelector(".menu");
+
 navbarEmail.addEventListener("click", toggleDesktopMenu);
+
+mobileHamburger.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("hide")
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("hide")
 }
